f3d598d1d7a17e74af5b48c78ede9500776c3ac3
OmoH37 <sanievs@list.ru>
2019-06-06 18:04:20 +0300

    message

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int x;
+int y;
+void func()
+{
+	cout << "First num" << endl;
+	cin >> x;
+	cout << "Second num" << endl;
+	cin >> y;
+}
+int main()
+{
+	string operation;
+	cout << "+   -   *   /" << endl;
+	cin >> operation;
+
+	if (operation == "+") 
+	{
+		func();
+		cout << x + y << endl;
+		main();
+	}
+
+	if (operation == "-")
+	{
+		func();
+		cout << x - y << endl;
+		main();
+	}
+
+	if (operation == "*")
+	{
+		func();
+		cout << x * y << endl;
+		main();
+	}
+
+
+	if (operation == "/")
+	{
+		func();
+		cout << x / y << endl;
+		main();
+	}
+	return 0;
+}
+
